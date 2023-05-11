11d86836fd069a3acf3422ae03cf38195716ab9e
davlatsultonov <davlatsultonov2001@gmail.com>
2023-05-11 20:49:48 +0500

    project init

---
+++ main.js
@@ -0,0 +1,79 @@
+import "./style.scss";
+import "normalize.css/normalize.css";
+import Swiper from "swiper";
+import $ from 'jquery';
+window.jQuery = window.$ = $;
+
+$(document).ready(function () {
+    if ($('.work_box_cont_img').length > 2) workBlock()
+
+    if ($(window).width() > 380) {
+        const navigationSectionEl = $('.navigation-sections');
+        const distance = navigationSectionEl.offset().top;
+
+        placeNavigationSection(navigationSectionEl, distance);
+        $(window).scroll(() => placeNavigationSection(navigationSectionEl, distance));
+
+        $(document).on('click', 'a[href^="#"]', function (event) {
+            event.preventDefault();
+
+            $('html, body').animate({
+                scrollTop: $($.attr(this, 'href')).offset().top - ($(window).width() > 1024 ? 140 : 120)
+            }, 500);
+        });
+    }
+});
+
+function placeNavigationSection (el, distance) {
+    const styles = {
+        'transition': 'all 400ms ease-in-out'
+    }
+
+    el.css($(window).scrollTop() > distance + 80 ? {
+        ...styles,
+        'position': 'sticky',
+        'top': $(window).width() > 1024 ? 80 : ($(window).width() <= 380 ? 55 : 62),
+        'z-index': 100,
+    } : {
+        ...styles,
+        'position': 'static',
+        'top': 0,
+    });
+}
+
+function workBlock() {
+    let sliderWork = "";
+
+    function initSlider() {
+        if (sliderWork === "") {
+            sliderWork = new Swiper(".work_box_cont", {
+                wrapperClass: "work_box_cont_block",
+                slideClass: "work_box_cont_img",
+                speed: 6000,
+                freeModeMomentumRatio: 0.8,
+                freeModeMomentumVelocityRatio: 0.8,
+                freeModeMomentumBounceRatio: 1,
+                slidesPerView: "auto",
+                loop: true,
+                freeMode: true,
+                loopedSlides: 10,
+                autoplay: {
+                    delay: 0.5,
+                    disableOnInteraction: false,
+                },
+                pauseOnFocus: true,
+                pauseOnHover: true,
+            });
+        }
+    }
+
+    initSlider();
+
+    window.addEventListener('resize', updateSlider);
+
+    function updateSlider() {
+        if (sliderWork !== "") {
+            sliderWork.update();
+        }
+    }
+}
