e22c4c215bb6b2b266ffe5f58ef44d872264bd99
davlatsultonov <davlatsultonov2001@gmail.com>
2023-05-12 12:14:41 +0500

    refactor vite config file
    - move images from public to assets folder

--- vite.config.js
+++ vite.config.js
@@ -1,3 +1,18 @@
-export default {
-    base: '/dermotologist-portfolio/'
-}
+import {defineConfig} from "vite";
+
+export default defineConfig(({ command, mode, ssrBuild }) => {
+    const isProduction = mode === 'production';
+
+    console.log(command)
+
+    return {
+        base: isProduction ? '/dermotologist-portfolio/' : '',
+        server: {
+            open: isProduction ? "/dermotologist-portfolio/index.html" : "index.html",
+            hot: true,
+        },
+        preview: {
+            open: isProduction ? "/dermotologist-portfolio/index.html" : "index.html"
+        }
+    }
+});
