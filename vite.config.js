ee37e22d6e3f0a830c91e9a0e6fb80595a150205
davlatsultonov <davlatsultonov2001@gmail.com>
2024-01-14 14:10:36 +0500

    feat(vite.config): changed config for multiple pages

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,25 @@
+import { resolve } from "path";
 import {defineConfig} from "vite";
 
 export default defineConfig(({ command, mode, ssrBuild }) => {
     const isProduction = mode === 'production';
 
     return {
-        base: isProduction ? '/dermotologist-portfolio/' : '',
+        base: isProduction ? 'dermotologist-portfolio/' : '',
         server: {
-            open: isProduction ? "/dermotologist-portfolio/index.html" : "index.html",
+            open: isProduction ? "dermotologist-portfolio/" : "index.html",
             hot: true,
         },
         preview: {
-            open: isProduction ? "/dermotologist-portfolio/index.html" : "index.html"
+            open: isProduction ? "dermotologist-portfolio/" : "index.html"
+        },
+        build: {
+            rollupOptions: {
+                input: {
+                    main: resolve(__dirname, 'index.html'),
+                    about: resolve(__dirname, 'about.html')
+                }
+            }
         }
     }
 });
